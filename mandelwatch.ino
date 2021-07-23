e0158fab7ed6cf9aaa76deff108e5f91c70e6ec8
Mike Redrobe <mike@redrobe.net>
2021-07-23 10:37:00 +0100

    Update mandelwatch.ino

--- mandelwatch.ino
+++ mandelwatch.ino
@@ -1,21 +1,4 @@
-// Demo based on:
-// UTFT_Demo_320x240 by Henning Karlsen
-// web: http://www.henningkarlsen.com/electronics
-//
-/*
 
-  This sketch uses the GLCD and font 2 only.
-
-  Make sure all the display driver and pin comnenctions are correct by
-  editting the User_Setup.h file in the TFT_eSPI library folder.
-
-  Note that yield() or delay(0) must be called in long duration for/while
-  loops to stop the ESP8266 watchdog triggering.
-
-  #########################################################################
-  ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
-  #########################################################################
-*/
 
 #include "config.h"
 
